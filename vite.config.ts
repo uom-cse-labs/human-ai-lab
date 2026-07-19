58d933bc8d4e4e6c0fe9bb31fbe35ae088be59ba
dulranga <dulranga.webdesign@gmail.com>
2026-07-19 14:33:15 +0530

    added github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   resolve: { tsconfigPaths: true },
   plugins: [devtools(), tailwindcss(), TanStackRouterVite(), viteReact()],
 })
